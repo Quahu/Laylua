4275e7ce724d2395a8cc1053a83dc16d5d73acab
Quahu <quahuu@gmail.com>
2024-02-28 22:28:44 +0100

    Added helper extension methods for setting common descriptors.

--- src/Laylua/Library/Marshaler/UserData/Descriptor/Provider/UserDataDescriptorProviderExtensions.cs
+++ src/Laylua/Library/Marshaler/UserData/Descriptor/Provider/UserDataDescriptorProviderExtensions.cs
@@ -6,4 +6,20 @@ public static class UserDataDescriptorProviderExtensions
     {
         provider.SetDescriptor(typeof(T), descriptor);
     }
+
+    public static void SetInstanceDescriptor<T>(this UserDataDescriptorProvider provider,
+        TypeMemberProvider? memberProvider = null,
+        UserDataNamingPolicy? namingPolicy = null,
+        CallbackUserDataDescriptorFlags disabledCallbacks = CallbackUserDataDescriptorFlags.None)
+    {
+        provider.SetDescriptor(typeof(T), new InstanceTypeUserDataDescriptor(typeof(T), memberProvider, namingPolicy, disabledCallbacks));
+    }
+
+    public static void SetDefinitionDescriptor<T>(this UserDataDescriptorProvider provider,
+        TypeMemberProvider? memberProvider = null,
+        UserDataNamingPolicy? namingPolicy = null,
+        CallbackUserDataDescriptorFlags disabledCallbacks = CallbackUserDataDescriptorFlags.None)
+    {
+        provider.SetDescriptor(typeof(T), new DefinitionTypeUserDataDescriptor(typeof(T), memberProvider, namingPolicy, disabledCallbacks));
+    }
 }
