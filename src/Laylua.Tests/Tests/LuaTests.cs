5569e362e3f106319bc883094b2e3bd9d98452d9
Quahu <quahuu@gmail.com>
2024-03-03 02:57:32 +0100

    Added tests for MainThread and Globals.

--- src/Laylua.Tests/Tests/LuaTests.cs
+++ src/Laylua.Tests/Tests/LuaTests.cs
@@ -34,5 +34,39 @@ namespace Laylua.Tests
             // Assert
             Assert.That(type, Is.EqualTo(LuaType.UserData));
         }
+
+        [Test]
+        public unsafe void MainThread_ReturnsValidEntity()
+        {
+            // Arrange
+            var mainThread = lua.MainThread;
+            var mainThreadReference = LuaReference.GetReference(mainThread);
+
+            // Act
+            lua_rawgeti(L, LuaRegistry.Index, mainThreadReference);
+            var type = lua_type(L, -1);
+            lua_pop(L);
+
+            // Assert
+            Assert.That(LuaRegistry.IsPersistentReference(mainThreadReference), Is.True);
+            Assert.That(type, Is.EqualTo(LuaType.Thread));
+        }
+
+        [Test]
+        public unsafe void Globals_ReturnsValidEntity()
+        {
+            // Arrange
+            var globals = lua.Globals;
+            var globalsReference = LuaReference.GetReference(globals);
+
+            // Act
+            lua_rawgeti(L, LuaRegistry.Index, globalsReference);
+            var type = lua_type(L, -1);
+            lua_pop(L);
+
+            // Assert
+            Assert.That(LuaRegistry.IsPersistentReference(globalsReference), Is.True);
+            Assert.That(type, Is.EqualTo(LuaType.Table));
+        }
     }
 }
