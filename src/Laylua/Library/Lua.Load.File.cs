d640f5372b8682f6767d984cdd94f71d342ea4c8
Quahu <quahuu@gmail.com>
2024-09-04 21:11:11 +0200

    Add Lua.LoadFile stream overload

--- src/Laylua/Library/Lua.Load.File.cs
+++ src/Laylua/Library/Lua.Load.File.cs
@@ -10,6 +10,7 @@ public unsafe partial class Lua
 
     /// <summary>
     ///     Loads a Lua chunk from the specified file.
+    ///     This method will skip the UTF-8 byte-order mark (BOM).
     ///     <para> See <a href="https://www.lua.org/manual/5.4/manual.html#3.3.2">Lua manual</a> for more information about chunks. </para>
     /// </summary>
     /// <param name="filePath"> The path to the file to load the Lua chunk from.  </param>
@@ -19,34 +20,40 @@ public unsafe partial class Lua
     /// </returns>
     public LuaFunction LoadFile(string filePath, ReadOnlySpan<char> chunkName = default)
     {
-        using (Stack.SnapshotCount())
+        using (var fileStream = File.OpenRead(filePath))
         {
-            LoadFilePath(filePath, chunkName);
-            return Stack[-1].GetValue<LuaFunction>()!;
+            return LoadFile(fileStream, chunkName);
         }
     }
 
-    private void LoadFilePath(string filePath, ReadOnlySpan<char> chunkName)
+    /// <summary>
+    ///     Loads a Lua chunk from the specified file.
+    ///     This method will skip the UTF-8 byte-order mark (BOM).
+    ///     <para> See <a href="https://www.lua.org/manual/5.4/manual.html#3.3.2">Lua manual</a> for more information about chunks. </para>
+    /// </summary>
+    /// <param name="stream"> The stream with the file contents to load the Lua chunk from.  </param>
+    /// <param name="chunkName"> The name of the chunk. If not specified, will default to <see cref="FileStream.Name"/> if <paramref name="stream"/> is a <see cref="FileStream"/>. </param>
+    /// <returns>
+    ///     A <see cref="LuaFunction"/> representing the loaded chunk.
+    /// </returns>
+    public LuaFunction LoadFile(Stream stream, ReadOnlySpan<char> chunkName = default)
     {
-        using (var fileStream = File.OpenRead(filePath))
-        {
-            SkipPreamble(fileStream);
+        SkipPreamble(stream);
 
-            LoadStream(fileStream, chunkName.Length == 0 ? $"@{fileStream.Name}" : chunkName);
-        }
+        return Load(stream, chunkName.Length == 0 && stream is FileStream fileStream ? $"@{fileStream.Name}" : chunkName);
 
-        static void SkipPreamble(FileStream fileStream)
+        static void SkipPreamble(Stream stream)
         {
-            if (fileStream.Length >= Utf8Preamble.Length)
+            if (stream.Length < Utf8Preamble.Length)
+                return;
+
+            var preambleBuffer = (stackalloc byte[4])[..Utf8Preamble.Length];
+            var bytesRead = stream.Read(preambleBuffer);
+            Debug.Assert(bytesRead == Utf8Preamble.Length);
+
+            if (preambleBuffer.SequenceEqual(Utf8Preamble))
             {
-                var preambleBuffer = (stackalloc byte[4])[..Utf8Preamble.Length];
-                var bytesRead = fileStream.Read(preambleBuffer);
-                Debug.Assert(bytesRead == Utf8Preamble.Length);
-
-                if (preambleBuffer.SequenceEqual(Utf8Preamble))
-                {
-                    fileStream.Seek(Utf8Preamble.Length, SeekOrigin.Begin);
-                }
+                stream.Seek(Utf8Preamble.Length, SeekOrigin.Begin);
             }
         }
     }
