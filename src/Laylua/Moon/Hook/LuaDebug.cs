da12e858bea71802404a837b883390b285eeef88
Quahu <quahuu@gmail.com>
2025-01-28 16:22:24 +0100

    Add some xmldocs to LuaDebug

--- src/Laylua/Moon/Hook/LuaDebug.cs
+++ src/Laylua/Moon/Hook/LuaDebug.cs
@@ -15,6 +15,9 @@ public unsafe ref struct LuaDebug
     /// </summary>
     public readonly LuaEvent Event => ActivationRecord->@event;
 
+    /// <summary>
+    ///     Gets a reasonable name for the given function.
+    /// </summary>
     public LuaString FunctionName
     {
         get
@@ -24,6 +27,10 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets a value explaining the name field.
+    ///     This can be "global", "local", "method", "field", "upvalue", or "", according to how the function was called.
+    /// </summary>
     public LuaString FunctionTypeName
     {
         get
@@ -33,6 +40,9 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets the string "Lua" if the function is a Lua function, "C" if it is a C function, "main" if it is the main part of a chunk.
+    /// </summary>
     public LuaString ChunkName
     {
         get
@@ -42,6 +52,12 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets the source of the chunk that created the function.
+    ///     If source starts with a '@', it means that the function was defined in a file where the file name follows the '@'.
+    ///     If source starts with a '=', the remainder of its contents describes the source in a user-dependent manner.
+    ///     Otherwise, the function was defined in a string where source is that string.
+    /// </summary>
     public LuaString Source
     {
         get
@@ -51,6 +67,10 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets the current line where the given function is executing.
+    ///     When no line information is available, this returns <c>-1</c>.
+    /// </summary>
     public int CurrentLineNumber
     {
         get
@@ -60,6 +80,9 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets the line number where the definition of the function starts.
+    /// </summary>
     public int FunctionDefinitionStartLineNumber
     {
         get
@@ -69,6 +92,9 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets the line number where the definition of the function ends.
+    /// </summary>
     public int FunctionDefinitionEndLineNumber
     {
         get
@@ -78,6 +104,9 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets the number of upvalues of the function.
+    /// </summary>
     public int FunctionUpvalueCount
     {
         get
@@ -87,6 +116,9 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets the number of parameters of the function (always <c>0</c> for C functions).
+    /// </summary>
     public int FunctionParameterCount
     {
         get
@@ -96,6 +128,9 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets whether the function is a variadic function (always true for C functions).
+    /// </summary>
     public bool IsFunctionVariadic
     {
         get
@@ -105,6 +140,10 @@ public unsafe ref struct LuaDebug
         }
     }
 
+    /// <summary>
+    ///     Gets whether the function invocation was called by a tail call.
+    ///     In this case, the caller of this level is not in the stack.
+    /// </summary>
     public bool IsTailCall
     {
         get
@@ -114,7 +153,48 @@ public unsafe ref struct LuaDebug
         }
     }
 
-    // TODO: locals
+    // TODO: xmldocs
+    /// <summary>
+    ///     Gets the index in the stack of the first value being "transferred", that is,
+    ///     parameters in a call or return values in a return. (The other values are in consecutive indices.)
+    ///     Using this index, you can access and modify these values through lua_getlocal and lua_setlocal.
+    ///     This field is only meaningful during a call hook, denoting the first parameter, or a return hook, denoting the first value being returned.
+    ///     (For call hooks, this value is always 1.)
+    /// </summary>
+    public int FirstTransferredValueIndex
+    {
+        get
+        {
+            EnsureInfoRetrieved(LuaDebugInfo.Transfer);
+            return ActivationRecord->ftransfer;
+        }
+    }
+
+    // TODO: xmldocs
+    /// <summary>
+    ///     The number of values being transferred (see previous item).
+    ///     (For calls of Lua functions, this value is always equal to nparams.)
+    /// </summary>
+    public int TransferredValueCount
+    {
+        get
+        {
+            EnsureInfoRetrieved(LuaDebugInfo.Transfer);
+            return ActivationRecord->ntransfer;
+        }
+    }
+
+    /// <summary>
+    ///     Gets a "printable" version of <see cref="Source"/>, to be used in error messages.
+    /// </summary>
+    public LuaString ShortSource
+    {
+        get
+        {
+            EnsureInfoRetrieved(LuaDebugInfo.Source);
+            return new(ActivationRecord->short_src, LUA_IDSIZE);
+        }
+    }
 
     internal readonly lua_Debug* ActivationRecord;
 
