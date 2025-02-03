8e75d74ae902e6db07067577bc7279d815d3e05a
Quahu <quahuu@gmail.com>
2025-02-03 19:38:48 +0100

    Make LuaChildThread not return Lua

--- src/Laylua/Library/LuaChildThread.cs
+++ src/Laylua/Library/LuaChildThread.cs
@@ -6,7 +6,7 @@ namespace Laylua;
 internal sealed unsafe class LuaChildThread : LuaThread
 {
     /// <inheritdoc/>
-    public override Lua MainThread => _mainThread;
+    public override LuaThread MainThread => Lua.FromExtraSpace(State.L).MainThread;
 
     /// <inheritdoc />
     public override LuaTable Globals => MainThread.Globals;
@@ -14,14 +14,10 @@ internal sealed unsafe class LuaChildThread : LuaThread
     /// <inheritdoc />
     internal override LuaMarshaler Marshaler => MainThread.Marshaler;
 
-    private Lua _mainThread = null!;
-
     internal void Initialize(lua_State* L, int reference)
     {
         Stack = new LuaStack(this);
         State = new LuaState(L);
         Reference = reference;
-
-        _mainThread = Laylua.Lua.FromExtraSpace(L);
     }
 }
